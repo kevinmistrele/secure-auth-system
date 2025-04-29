c7b07189fda87d541055a650f5bf02244d6ee76d
kevin <kevintavaresmistrele@gmail.com>
2025-04-29 15:51:07 -0300

    feat: adding .env to gitignore

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -5,7 +5,7 @@ const useApiService = () => {
     const { user } = useAuth();
 
     const axiosInstance = axios.create({
-        baseURL: 'http://localhost:3001/api',
+        baseURL: import.meta.env.VITE_API_URL,
     });
 
     if (user && user.token) {
